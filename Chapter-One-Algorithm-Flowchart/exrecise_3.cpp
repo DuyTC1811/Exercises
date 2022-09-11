673e3089ba23234555ef578fab5b2d9fc2dbb6e5
DuyTC <duytc@DuyTCs-MacBook-Pro.local>
2022-09-11 23:19:04 +0700

    exercise 3

---
+++ Chapter-One-Algorithm-Flowchart/exrecise_3.cpp
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main() {
+    int n;
+    do {
+        printf("Enter number : "); 
+        scanf("%d", &n);
+        if (n < 1) {
+            printf("pease enter a positive >= 1\n");
+        }
+    } while (n < 1);
+
+    float sum = 0;
+    for (int i = 1; i <= n; ++i) {
+        sum += (1.0 / i);
+    }
+    printf("Sum 1/2 + 1/3 is = %.2f", sum);
+    return 0;
+}
