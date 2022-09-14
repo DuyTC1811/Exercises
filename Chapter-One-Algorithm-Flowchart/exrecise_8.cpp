544909ec1a4c76b695e3c784a56bb17c482e69a1
DuyTC <duytc@DuyTCs-MacBook-Pro.local>
2022-09-15 00:03:14 +0700

    exrecise 8

---
+++ Chapter-One-Algorithm-Flowchart/exrecise_8.cpp
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main() {
+    int n;
+    do {
+        printf("Enter number : ");
+        scanf("%d", &n);
+        if (n < 1) {
+            printf("please enter a positive > 1\n");
+        }
+    } while (n < 1);
+
+    float sum = 0;
+    for (int i = 1; i <= n; ++i) {
+        sum += (float) i + 1 / (i + 2);
+    }
+    printf("Total 1/2 + 3/4...is =%.2f", sum);
+    return 0;
+}
