fd86c41a3dca7df10dcadb4e157393d2c4428498
DuyTC <duytc@DuyTCs-MacBook-Pro.local>
2022-09-17 11:21:43 +0700

    exrecise 13

---
+++ Chapter-One-Algorithm-Flowchart/exrecise_13.cpp
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <math.h>
+
+int main() {
+    int x;
+    int n;
+    printf("Enter number x : ");
+    scanf("%d", &x);
+    printf("Enter number n : ");
+    scanf("%d", &n);
+
+    float sum = 0, T = 1;
+    for (int i = 1; i <= n; ++i) {
+        T = pow(x, (i * 2));
+        sum = sum + T;
+    }
+
+    printf("Total x ^ 2 + x ^ 4... is = %f", sum);
+    return 0;
+}
