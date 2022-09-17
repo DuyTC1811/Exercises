d6e062ecc4b4ea3b1e49fc88eaf91a180b1b0563
DuyTC <duytc@DuyTCs-MacBook-Pro.local>
2022-09-17 11:44:20 +0700

    exrecise 14

---
+++ Chapter-One-Algorithm-Flowchart/exrecise_14.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <math.h>
+
+using namespace std;
+
+int main() {
+    int x;
+    int n;
+
+    cout << "Enter number x : " << endl;
+    cin >> x;
+    cout << "Enter number n : " << endl;
+    cin >> n;
+
+    double sum = 0, T = 1;
+    for (int i = 1; i <= n; ++i) {
+        T = T + pow(x, i * 2 - 1);
+        sum = sum + T;
+    }
+
+    cout << "Total ^ * 3 + x ^ 5... is = " << sum << endl;
+    return 0;
+}
