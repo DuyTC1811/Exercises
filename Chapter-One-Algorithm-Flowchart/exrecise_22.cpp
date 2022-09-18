563514356bbae1485259d12cc926452309b3f95e
DuyTC <duytc@DuyTCs-MacBook-Pro.local>
2022-09-18 21:32:20 +0700

    exrecise 22

---
+++ Chapter-One-Algorithm-Flowchart/exrecise_22.cpp
@@ -0,0 +1,23 @@
+#include <iostream>	
+using namespace std;
+
+int main() {
+    int n;
+    do {
+        cout  << "Enter number n : ";
+        cin >> n;
+        if (n < 1) {
+            cout << "please enter a positive > 1" << endl; 
+        }
+    } while (n < 1);
+
+    int sum = 1;
+    for (int i = 1; i <= n; ++i) {
+        if (i % 2 == 0) {
+            printf("%3d", i);
+            sum = sum * i; 
+        }
+    }
+    cout << "\nTotal product of a positive integer " << sum << endl;
+    return 0;
+}
