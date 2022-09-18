319dfe836b8649fc67757b31557f5c7dba132413
DuyTC <duytc@DuyTCs-MacBook-Pro.local>
2022-09-18 22:33:27 +0700

    exrecise 28

---
+++ Chapter-One-Algorithm-Flowchart/exrecise_28.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    do {
+        cout << "Enter number n : " << endl;
+        cin >> n;
+        if (n < 1) {
+            cout << "please enter a positive > 1" << endl;
+        }
+    } while (n < 1);
+
+
+    int sum = 0;
+    for (int i = 1; i <= n; ++i) {
+       if (n % i == 0) {
+            printf("%3d ", i);
+            sum = sum + i;
+        } 
+    }
+    cout << "\nTotal = " << sum << endl;
+    return 0;
+}
