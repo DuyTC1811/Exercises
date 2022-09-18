ec0aea026a28e1e9ea27f56a75bb6bd74f8f52e7
DuyTC <duytc@DuyTCs-MacBook-Pro.local>
2022-09-18 22:11:02 +0700

    exrecise 22

--- Chapter-One-Algorithm-Flowchart/exrecise_22.cpp
+++ Chapter-One-Algorithm-Flowchart/exrecise_22.cpp
@@ -13,7 +13,7 @@ int main() {
 
     int sum = 1;
     for (int i = 1; i <= n; ++i) {
-        if (i % 2 == 0) {
+        if (n % i == 0) {
             printf("%3d", i);
             sum = sum * i; 
         }
