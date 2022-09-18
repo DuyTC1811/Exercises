061b9d835a08638ad1336ef94b890fb8271dee87
DuyTC <duytc@DuyTCs-MacBook-Pro.local>
2022-09-18 22:22:33 +0700

    exrecise 26

--- Chapter-One-Algorithm-Flowchart/exrecise_26.cpp
+++ Chapter-One-Algorithm-Flowchart/exrecise_26.cpp
@@ -5,7 +5,7 @@ int main() {
     int n;
     do {
         cout << "Enter number n : " << endl;
-       cin >> n; 
+        cin >> n; 
         if (n < 1) {
             cout << "please enter a positive > 1" << endl;
         }
@@ -13,9 +13,11 @@ int main() {
 
     int sum = 0;
     for (int i = 1; i <= n; ++i) {
-        if (i % 2 != 0) {
-            printf("%3d ", i);
-            sum = sum + i;
+        if (n % i == 0) {
+            if (i % 2 != 0) {
+                printf("%3d ", i);
+                sum = sum * i;
+            }
         }
     }
     cout << "\nTotal odd divisor" << sum << endl;
